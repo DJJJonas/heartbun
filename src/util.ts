abfb9242a531dad10c9c7be003db75928745e81c
DJJJonas <jonasmelo@duck.com>
2024-04-25 21:57:05 -0300

    add range function

--- src/util.ts
+++ src/util.ts
@@ -1,13 +1,18 @@
 /** Random number generator. min and max are inclusive. */
-export function rng(min: number, max: number) {
+export function rng(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 /** shuffle given array using fisher-yates algorithm */
-export function shuffle(arr: Array<any>) {
+export function shuffle(arr: Array<any>): void {
   const len = arr.length - 1;
   for (let i = 0; i < arr.length; i++) {
     const n = rng(i, len);
     [arr[i], arr[n]] = [arr[n], arr[i]];
   }
+}
+
+/** Python-like range function that returns an array from `0` to `count - 1` */
+export function range(count: number): Array<number> {
+  return Array.from(Array(5), (_, i) => i);
 }
