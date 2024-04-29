14cb41659dbb9a71cf18837dac885f8e426bbfa7
DJJJonas <jonasmelo@duck.com>
2024-04-28 21:13:49 -0300

    redo part of the mock

--- src/tests/1.basic.test.ts
+++ src/tests/1.basic.test.ts
@@ -20,16 +20,15 @@ test("mock", () => {
   // want to switch
   // In the following case, both players
   // will switch their whole hand
-  game.mulligan(
-    p1mul.map((c) => c.id!),
-    p2mul.map((c) => c.id!)
-  );
+  const msg1 = game.player1Messages.pop();
+  const msg2 = game.player2Messages.pop();
+
   const p1 = game.players[0];
   const p2 = game.players[1];
   // Players hands
   const p1hand: Array<ICard> = p1.hand;
   const p2hand: Array<ICard> = p2.hand;
-  expect(game.currentPlayer).toBe(0); // First player's turn
+  expect(game.turnPlayerIndex).toBe(0); // First player's turn
   // Player one check
   expect(p1.health).toBe(30);
   expect(p1.armor).toBe(0);
@@ -48,11 +47,11 @@ test("mock", () => {
   game.playCard(p1hand[0].id!);
   // Player 1 end turn
   game.endTurn();
-  expect(game.currentPlayer).toBe(1); // Second player's turn
+  expect(game.turnPlayerIndex).toBe(1); // Second player's turn
   // Now player two will play a card
   game.playCard(p2hand[0].id!);
   // End game and set player one to winner
-  game.setWinner(0);
+  game.forceWinner(0);
 });
 
 function sample() {
