eba2a835498f51a4eee0615fbb3e79d3a0ee3cf3
DJJJonas <jonasmelo@duck.com>
2024-04-28 21:14:27 -0300

    add base engine message for engine

---
+++ src/interfaces/engine_message.ts
@@ -0,0 +1,19 @@
+import type ICard from "./card";
+
+/**
+ * Type
+ * - request - game state is paused until player send the requested action
+ * - response - game state changed
+ *
+ * Action
+ * - mulligan - player needs to choose which cards to mulligate
+ * - free - player can play any available action (e.g. attack, hero power...)
+ *
+ * Cards
+ * associated cards with the title
+ */
+export interface EngineMessage {
+  type: "request" | "response";
+  action: "mulligan" | "free";
+  cards: Array<ICard>;
+}
