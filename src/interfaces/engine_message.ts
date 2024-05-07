0b01bd3c87b1b8ff0cfec453ba9d049eeff3e81b
DJJJonas <jonasmelo@duck.com>
2024-05-07 19:17:53 -0300

    add player to engine message

--- src/interfaces/engine_message.ts
+++ src/interfaces/engine_message.ts
@@ -1,21 +1,10 @@
 import type { EngineMessageAction } from "@/types";
 import type Card from "./card";
 
-/**
- * Type
- * - request - game state is paused until player send the requested action
- * - response - game state changed
- *
- * Action
- * - mulligan - player needs to choose which cards to mulligate
- * - free - player can play any available action (e.g. attack, hero power...)
- *
- * Cards
- * associated cards with the title
- */
 export interface EngineMessage {
   action: EngineMessageAction;
   type?: "request" | "response";
   cards?: Array<Card>;
   ids?: Array<number>;
+  player?: number;
 }
