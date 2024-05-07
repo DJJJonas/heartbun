b32d35286e59326addb5fce8a1b56a9a9fd13f37
DJJJonas <jonasmelo@duck.com>
2024-05-06 22:08:44 -0300

    update test

--- src/tests/1.basic.test.ts
+++ src/tests/1.basic.test.ts
@@ -13,13 +13,7 @@ test("mock", () => {
   const game = new Engine(player1deck, player2deck);
   // Player 1 will receive 3 cards for mulligan
   // Player 2 will receive 4 cards for mulligan
-  const [p1mul, p2mul]: Array<Array<Card>> = game.start();
-  expect(p1mul).toBeArrayOfSize(3);
-  expect(p2mul).toBeArrayOfSize(4);
-  // `mulligan` will receive the id of the cards the player
-  // want to switch
-  // In the following case, both players
-  // will switch their whole hand
+  game.start();
   const msg1 = game.player1Messages.pop();
   const msg2 = game.player2Messages.pop();
 
@@ -28,6 +22,23 @@ test("mock", () => {
   // Players hands
   const p1hand: Array<Card> = p1.hand;
   const p2hand: Array<Card> = p2.hand;
+  // TODO
+  // `mulligan` will receive the id of the cards the player
+  // want to switch
+  // In the following case, both players
+  // will switch their whole hand
+  game.send({
+    action: "mulligan",
+    ids: p1hand.map((card) => card.id!),
+    // player: 0, // TODO
+  });
+  game.send({
+    action: "mulligan",
+    ids: p2hand.map((card) => card.id!),
+    // player: 1, // TODO
+  });
+  expect(p1hand).toBeArrayOfSize(3);
+  expect(p2hand).toBeArrayOfSize(4);
   expect(game.turnPlayerIndex).toBe(0); // First player's turn
   // Player one check
   expect(p1.health).toBe(30);
@@ -44,14 +55,18 @@ test("mock", () => {
   // PlayCard will receive the id of the card
   // and by default the player of the current
   // turn will be the one who played
-  game.playCard(p1hand[0].id!);
+  //// game.playCard(p1hand[0].id!);
+  game.send({
+    action: "play",
+    cards: [],
+  });
   // Player 1 end turn
-  game.endTurn();
+  // game.endTurn(); // TODO send "endturn" message
   expect(game.turnPlayerIndex).toBe(1); // Second player's turn
   // Now player two will play a card
-  game.playCard(p2hand[0].id!);
+  // game.playCard(p2hand[0].id!);
   // End game and set player one to winner
-  game.forceWinner(0);
+  // game.forceWinner(0);
 });
 
 function sample() {
@@ -63,6 +78,7 @@ function sampleDeck(): Deck {
   // @ts-expect-error
   let deck: Deck = { cards: [] };
   // Fill deck with 30 1/1's
-  for (let i = 0; i < 30; i++) deck.cards.push(ArgentSquire.copy());
+  for (let i = 0; i < 30; i++)
+    deck.cards.push(Object.assign({ ...ArgentSquire }));
   return deck;
 }
