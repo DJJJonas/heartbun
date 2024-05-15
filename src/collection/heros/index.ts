07f85e6eee832792bce2a039f21001cf7d8cad55
DJJJonas <jonasmelo@duck.com>
2024-05-14 22:40:13 -0300

    add heroes index

---
+++ src/collection/heros/index.ts
@@ -0,0 +1,21 @@
+import Malfurion from "./Druid";
+import Hexxar from "./Hunter";
+import Jaina from "./Mage";
+import Uther from "./Paladin";
+import Anduin from "./Priest";
+import Valeera from "./Rogue";
+import Thrall from "./Shaman";
+import GulDan from "./Warlock";
+import Garrosh from "./Warrior";
+
+export default [
+  Malfurion,
+  Hexxar,
+  Jaina,
+  Uther,
+  Anduin,
+  Valeera,
+  Thrall,
+  GulDan,
+  Garrosh,
+];
