ccb481b67302cc58762bd7aa8ac257c32ca4e0a8
DJJJonas <jonasmelo@duck.com>
2024-05-20 23:21:47 -0300

    replace func signature to EventContext

--- src/engine/event_manager.ts
+++ src/engine/event_manager.ts
@@ -1,7 +1,6 @@
 import type { EventContext } from "@/interfaces/event_context";
 import type Player from "@/interfaces/player";
 import type { EngineEventName } from "@/types";
-import { removeCardId } from "@/util";
 import type Engine from ".";
 
 /**
@@ -10,80 +9,88 @@ import type Engine from ".";
 export default class EventManager {
   get allCards() {
     return this.engine.players
-      .map((p) => [...p.hand, ...p.deck, ...p.minions]) // TODO: add p.hero after impl heroes
+      .map((player) => this.allCardsFrom(player)) // TODO: add p.hero after impl heroes
       .flat();
   }
 
   constructor(private engine: Engine) {}
 
+  allCardsFrom(player: Player) {
+    return [...player.hand, ...player.deck, ...player.minions];
+  }
+
   startOfGame() {
-    this.allCards.forEach((source) => {
-      this.trigger("startOfGame", { source });
+    this.engine.players.forEach((player) => {
+      this.allCardsFrom(player).forEach((source) => {
+        this.trigger("startOfGame", { player, source });
+      });
     });
   }
 
   // plays card from player's hand and returns if the action was completed
-  play(player: Player, id: number) {
-    const cardIndex = player.hand.findIndex((c) => c.id === id);
-    if (cardIndex < 0) return false;
+  play({ player, source }: EventContext) {
+    const srcIndex = player.hand.findIndex((c) => c === source);
+    if (srcIndex < 0) return false;
     // remove card from hand
-    const source = player.hand.splice(cardIndex, 1)[0];
+    player.hand.splice(srcIndex, 1)[0];
     // place card on board
     player.minions.push(source);
     // TODO trigger "minionSpawn" event
-    this.trigger("battlecry", { source });
+    this.trigger("battlecry", { player, source });
 
     return true;
   }
 
   endTurn() {
     const player = this.engine.turnPlayer;
-    this.engine.turn++;
     const source = player.hero;
+    this.engine.turn++;
     this.engine.turnPlayerIndex = Number(!this.engine.turnPlayerIndex);
-    this.trigger("endOfTurn", { source });
+    this.trigger("endOfTurn", { player, source });
 
-    this.addMaxMana(player);
-    this.refreshMana(player);
-    this.trigger("manaGain", { source });
+    this.addMaxMana({ player, source });
+    this.refreshMana({ player, source });
+    this.trigger("manaGain", { player, source });
   }
 
-  trigger(eventName: EngineEventName, context: EventContext) {
+  trigger(eventName: EngineEventName, ctx: EventContext) {
     this.allCards.forEach((c) => {
       const enchants = [c.defaultEnchantments, c.enchantments].flat();
       enchants.forEach((enc) => {
         const evs = enc?.events.get(eventName);
-        evs?.forEach((ev) => ev(context, this));
+        evs?.forEach((ev) => ev(ctx, this));
       });
     });
   }
 
-  addMaxMana(player: Player) {
+  addMaxMana({ player }: EventContext) {
+    if (!player) return;
     if (player.maxMana <= player.maxManaLimit) {
       player.maxMana++;
     }
   }
 
-  refreshMana(player: Player) {
+  refreshMana({ player }: EventContext) {
     player.mana = player.maxMana;
   }
 
-  // TODO replace function signature to `attack{ctx: EventContext}`, and rename EventContext to Context
-  attack(sourceId: number, targetId: number) {
-    const player = this.engine.turnPlayer;
-    const opponent = this.engine.opponentPlayer;
-    const source = removeCardId([...player.minions, player.hero], sourceId);
-    const target = removeCardId([...opponent.minions, opponent.hero], targetId);
+  // TODO rename EventContext to Context
+  attack({ player, source, target }: EventContext) {
     if (!source || !target) return;
     source.health! -= target.attack!;
     target.health! -= source.attack!;
     // TODO beforeAttack
     const damageDealt = source.attack;
-    this.trigger("attack", { source: source, damageDealt });
+    this.trigger("attack", { player, source, damageDealt });
     // TODO "attacked" && "beforeAttacked" event ?
   }
 
-  // TODO these functions
-  dealDamage(ctx: EventContext) {}
+  dealDamage({ player, source, target, damageDealt }: EventContext) {
+    if (!source || !target?.health || !damageDealt)
+      throw new Error("can't deal damage: missing params");
+    target.health -= damageDealt;
+    this.trigger("damage", { player, source, target, damageDealt });
+  }
+
   destroy(ctx: EventContext) {}
 }
