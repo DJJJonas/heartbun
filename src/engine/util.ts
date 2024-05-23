ec074bf18b5b0e46b9700ef5592d58a3710f11bd
DJJJonas <jonasmelo@duck.com>
2024-05-22 22:24:19 -0300

    fix health not found and add util func

--- src/engine/util.ts
+++ src/engine/util.ts
@@ -1,3 +1,4 @@
+import type Card from "@/interfaces/card";
 import type Deck from "@/interfaces/deck";
 import type Player from "@/interfaces/player";
 
@@ -6,11 +7,15 @@ export function newPlayer(deck: Deck): Player {
     deck: deck.cards,
     hero: deck.hero,
     hand: [],
-    health: 30,
     armor: 0,
     mana: 0,
     maxMana: 0,
     maxManaLimit: 10,
     minions: [],
+    messageChannel(_) {},
   };
+}
+
+export function hasAttackAndHealth(card: Card) {
+  return card.attack !== undefined && card.health !== undefined;
 }
