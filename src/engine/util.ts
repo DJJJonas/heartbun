1bdca219dd2838dad749a252638a7657232c5fa3
DJJJonas <jonasmelo@duck.com>
2024-06-05 23:54:04 -0300

    fix required deck length to play

--- src/engine/util.ts
+++ src/engine/util.ts
@@ -2,7 +2,7 @@ import type Card from "@/interfaces/card";
 import type Player from "@/interfaces/player";
 
 export function newPlayer(deck: Array<Card>): Player {
-  if (deck.length !== 31) throw new Error("deck is incomplete");
+  if (deck.length === 0) throw new Error("deck does not have a hero");
 
   return {
     hero: deck.shift()!,
