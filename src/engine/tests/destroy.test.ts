81606af32dd4bae6d90e32935b41d579786942ea
DJJJonas <jonasmelo@duck.com>
2024-06-07 23:31:31 -0300

    update destroy test

--- src/engine/tests/destroy.test.ts
+++ src/engine/tests/destroy.test.ts
@@ -13,8 +13,13 @@ test("mock", () => {
   game.start();
   game.send({ action: MessageAction.Mulligan, player: 0, ids: [] });
   game.send({ action: MessageAction.Mulligan, player: 1, ids: [] });
-  // TODO create spawn method and update this to game.eventManager.spawn(...)
-  game.players[0].minions.push(ysera);
+
+  game.eventManager.spawn({
+    player: game.players[0],
+    source: game.players[0].hero,
+    target: ysera,
+  });
+
   expect(game.players[0].minions).toBeArrayOfSize(1);
   game.eventManager.destroy({
     player: game.players[0],
