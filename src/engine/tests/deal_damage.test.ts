e7e647a0fb02d1d600cdc5cffca35e347f09b595
DJJJonas <jonasmelo@duck.com>
2024-06-25 23:31:55 -0300

    create deal damage test

---
+++ src/engine/tests/deal_damage.test.ts
@@ -0,0 +1,30 @@
+import Anduin from "@/collection/heros/Priest";
+import Ysera from "@/collection/set_Base/Neutral/Ysera";
+import type { EngineMessage } from "@/interfaces/engine_message";
+import { MessageAction } from "@/types";
+import { expect, test } from "bun:test";
+import Engine from "..";
+
+test("Deal damage event", () => {
+  const game = new Engine([{ ...Anduin }], [{ ...Anduin }]);
+  const ysera = { ...Ysera };
+  let lastMsg: EngineMessage;
+  game.players.forEach((p) => (p.messageChannel = (msg) => (lastMsg = msg)));
+  game.start();
+  game.send({ action: MessageAction.Mulligan, player: 0, ids: [] });
+  game.send({ action: MessageAction.Mulligan, player: 1, ids: [] });
+
+  game.eventManager.spawn({
+    player: game.players[0],
+    source: game.players[0].hero,
+    target: ysera,
+  });
+
+  expect(game.players[0].minions).toBeArrayOfSize(1);
+  game.eventManager.destroy({
+    player: game.players[0],
+    source: game.players[0].hero,
+    target: ysera,
+  });
+  expect(game.players[0].minions).toBeArrayOfSize(0);
+});
