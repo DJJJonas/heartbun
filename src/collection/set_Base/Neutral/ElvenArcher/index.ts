79f405ae6b9088c963d6940ceb816679d37d709e
DJJJonas <jonasmelo@duck.com>
2024-06-25 23:31:31 -0300

    implement elven archer

--- src/collection/set_Base/Neutral/ElvenArcher/index.ts
+++ src/collection/set_Base/Neutral/ElvenArcher/index.ts
@@ -1,5 +1,7 @@
-import type Card from "@/interfaces/card"
+import type Card from "@/interfaces/card";
+import { EventName, type Event } from "@/types";
 
+const events = new Map<EventName, Event[]>();
 const ElvenArcher: Card = {
   class: "neutral",
   rarity: undefined,
@@ -10,6 +12,21 @@ const ElvenArcher: Card = {
   text: "<b>Battlecry:</b> Deal 1 damage.",
   type: "minion",
   portrait: "",
-}
+  enchantments: [{ events }],
+  requestTargets(eng, thisID) {
+    const targets: Set<number> = new Set();
+    eng.players.forEach((player) => {
+      player.minions.forEach((minion) => targets.add(minion.id!));
+    });
+    targets.delete(thisID);
+    return targets;
+  },
+};
 
-export default ElvenArcher
+events.set(EventName.Battlecry, [
+  ({ player, source, target }, em) => {
+    em.dealDamage({ player, source, target, damage: 1 });
+  },
+]);
+
+export default ElvenArcher;
